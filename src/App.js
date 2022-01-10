91f0f3c20c8a8dd2e5bc9a78c5257217b2541c71
gal <galr2957@gmail.com>
2022-01-10 13:56:33 +0200

    addaed header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.components';
 import Shoppage from './pages/shop-page/shop-page.component';
 import {Route, Switch} from 'react-router-dom';
-
+import Header from './components/Header/header.component';
 import './app.css'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route  path='/shop' component={Shoppage}/>
